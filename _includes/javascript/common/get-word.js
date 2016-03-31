5cecfb5729d263552c618fdb466c35508e32ac64
pikesley <github@orgraphone.org>
2016-03-31 22:32:20 +0100

    Colours

--- _includes/javascript/common/get-word.js
+++ _includes/javascript/common/get-word.js
@@ -1,6 +1,10 @@
 {% include javascript/functions/get-url-car-crash.js %}
 
 var word = getUrlVars()['word']
+
 if(word === undefined) {
+  word = '{{ include.word }}'
+}
+if(word === '') {
   word = 'labs'
 }
--- _includes/javascript/logo-with-word.js
+++ _includes/javascript/logo-with-word.js
@@ -2,7 +2,7 @@
 
 {% include javascript/common/get-word.js %}
 
-var text = logo.text(word).move(232, 10).fill('{{ site.data.attributes.defaults.text-colour }}')
+var text = logo.text(word).move(232, 10).fill('{{ site.data.attributes.defaults.alt-colour }}')
 text.font({
   family: 'Helvetica Neue',
   size: 100,
