9eed1d109e954fd8a86abfd569f0ff216d521d2c
pikesley <github@orgraphone.org>
2016-04-01 09:36:11 +0100

    One more
    Closes #1

---
+++ _includes/javascript/logo-with-caps-word.js
@@ -0,0 +1,10 @@
+{% include javascript/glowing-logo.js %}
+
+{% include javascript/common/get-word.js %}
+
+var text = logo.text(word.toUpperCase()).move(232, 10).fill('{{ site.data.attributes.defaults.alt-colour }}')
+text.font({
+  family: 'Helvetica-Condensed',
+  size: 105,
+  anchor: 'middle'
+})
